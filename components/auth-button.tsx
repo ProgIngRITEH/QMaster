9c4d1ac7b254b75f66fb2934b625f4e3b5097f5c
Teo <vozilateo@gmail.com>
2026-04-15 01:44:35 +0200

    Name mapping in signup
    Added name mapping to signup and deleted unneeded files

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -13,7 +13,7 @@ export async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      <span className="text-sm">Hey, {user?.user_metadata?.full_name}!</span>
       <LogoutButton />
     </div>
   ) : (
